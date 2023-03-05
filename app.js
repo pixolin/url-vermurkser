3273bc153217b8f01e6fabc915ddbd5643d0d288
Bego Mario Garde <pixolin@pixolin.de>
2023-03-05 21:50:24 +0100

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+window.addEventListener('DOMContentLoaded', function () {
+	document
+		.getElementById('anonymizer')
+		.addEventListener('submit', (event) => {});
+});
+
+onsubmit = (event) => {
+	event.preventDefault();
+
+	userinput = document.querySelector('[name="urlanon"]').value;
+
+	if (!userinput.startsWith('http')) {
+		userinput = 'http://' + userinput;
+	}
+
+	try {
+		url = new URL(userinput);
+
+		toAnonymize = url.hostname;
+		parts = toAnonymize.split('.');
+		domain = parts[parts.length - 2];
+
+		if (domain.length < 8) {
+			domain = domain[0] + '***' + domain[domain.length - 1];
+		} else {
+			domain =
+				domain.slice(0, 3) +
+				'***' +
+				domain.slice(domain.length - 3, domain.length);
+		}
+	} catch (err) {
+		log.innerHTML =
+			'<p><mark>Keine gültige URL.</mark> Bitte Eingabe in der Form <pre>https://example.com/{path/to/file.html}</pre></p>' +
+			err +
+			'</p>';
+	}
+
+	parts[parts.length - 2] = domain;
+
+	newDomain =
+		url.protocol +
+		'//' +
+		parts.join('.') +
+		url.port +
+		url.pathname +
+		url.search +
+		url.hash;
+
+	navigator.clipboard.writeText(newDomain);
+
+	let output = '<p>In die Zwischenablage kopiert:</p>';
+	output += '<mark>' + newDomain + '</mark>';
+	log.innerHTML = output;
+};
---
+++ readme.md
@@ -0,0 +1,24 @@
+# URL-Vermurkser
+
+Webseite mit kurzem JavaScript zum anonymisieren von URLs.
+Aus `https://myexample.com/` wird `https://mye…ple.com`.
+
+Die Webseite verwendet ein Stylesheet aus dem OpenSource-Projekt [pico.css](https://picocss.com).
+
+## Installation und Anwendung
+
+```Bash
+git clone https://github.com/pixolin/url-vermurkser.git
+```
+
+Anschließend im Browser file://url-vermurkser/ öffnen. \
+URL im angegebenen Format einfügen, Button anklicken. \
+Die anonymisierte URL wird in die Zwischenablage kopiert.
+
+## Acknowledgement
+
+Remerciements particuliers à Lucas Larroche, le développeur du framework CSS [pico.css](https://picocss.com), un framework génial.
+
+---
+
+© 2023 Bego Mario Garde, MIT-License
