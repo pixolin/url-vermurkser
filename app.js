c6fd993b6ac9a8e47d142498878a3674be437133
Bego Mario Garde <pixolin@pixolin.de>
2023-04-28 19:12:11 +0200

    adds comments to JavaScript

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
+//run onsubmit if user submits form entry
 window.addEventListener('DOMContentLoaded', function () {
 	document
 		.getElementById('anonymizer')
-		.addEventListener('submit', (event) => {});
+		.addEventListener('submit', onsubmit );
 });
 
 onsubmit = (event) => {
 	event.preventDefault();
 
+  // read user input from form field
 	userinput = document.querySelector('[name="urlanon"]').value;
 
+  // check is url schema is provided or add one
 	if (!userinput.startsWith('http')) {
 		userinput = 'http://' + userinput;
 	}
 
+  // split URL in parts to get the hostname
 	try {
 		url = new URL(userinput);
 
@@ -20,6 +24,7 @@ onsubmit = (event) => {
 		parts = toAnonymize.split('.');
 		domain = parts[parts.length - 2];
 
+    // replace middle of hostname to anonymize URL
 		if (domain.length < 8) {
 			domain = domain[0] + '...' + domain[domain.length - 1];
 		} else {
@@ -29,6 +34,7 @@ onsubmit = (event) => {
 				domain.slice(domain.length - 3, domain.length);
 		}
 	} catch (err) {
+		// print error message, if no valid URL is entered
 		log.innerHTML =
 			'<p><mark>Keine gültige URL.</mark> Bitte Eingabe in der Form <pre>https://example.com/{path/to/file.html}</pre></p>' +
 			err +
@@ -37,6 +43,7 @@ onsubmit = (event) => {
 
 	parts[parts.length - 2] = domain;
 
+ // join parts of the URL
 	newDomain =
 		url.protocol +
 		'//' +
@@ -46,8 +53,10 @@ onsubmit = (event) => {
 		url.search +
 		url.hash;
 
+  // copy URL to clipboard
 	navigator.clipboard.writeText(newDomain);
 
+  // print anonymized URL
 	let output = '<p>In die Zwischenablage kopiert:</p>';
 	output += '<mark>' + newDomain + '</mark>';
 	log.innerHTML = output;
